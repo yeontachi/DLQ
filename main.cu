0f8b375705d72eb8eeb6efabfa83674a8218c34c
yeontachi <yeonjjhh@gmail.com>
2025-09-30 22:46:21 +0900

    NVIDIA GPU Architecture and Execution Pipeline(Volta, Ampere, Hopper)

--- main.cu
+++ main.cu
@@ -1,24 +1,39 @@
-// This is for the Test CUDA
-#include <cstdio>
+#include <iostream>
 #include <cuda_runtime.h>
 
-__global__ void add1(int*x){
-    x[0]+=1;
+// CUDA 커널: 100개의 스레드가 각자 1을 더하고 블록 내에서 합산
+__global__ void sumKernel(int *result){
+    __shared__ int shared[100]; // 공유 메모리 (스레드 100개용)
+
+    int tid = threadIdx.x;
+    shared[tid] = 1;  // 각 스레드가 1을 넣음
+
+    __syncthreads();  // 모든 스레드가 shared[]에 값을 넣을 때까지 대기
+
+    // 병렬 reduction (반으로 나누어 합치기)
+    for
 }
 
-int main() {
-    int h = 41, *d = nullptr;
-    cudaError_t err = cudaMalloc(&d, sizeof(int));
-    if (err != cudaSuccess) { printf("cudaMalloc error: %s\n", cudaGetErrorString(err)); return 1; }
+int main(void){
+    const int NUM_THREADS=100;
+    const int NUM_BLOCKS=1;
 
-    cudaMemcpy(d, &h, sizeof(int), cudaMemcpyHostToDevice);
-    add1<<<1,10>>>(d);
-    err = cudaDeviceSynchronize();
-    if (err != cudaSuccess) { printf("kernel error: %s\n", cudaGetErrorString(err)); return 1; }
+    int h_result = 0; // CPU 메모리
+    int *d_result;    // GPU 메모리
 
-    cudaMemcpy(&h, d, sizeof(int), cudaMemcpyDeviceToHost);
-    cudaFree(d);
+    // GPU 메모리 할당 및 초기화
+    cudaMalloc((void**)&d_result, sizeof(int));
+    cudaMemcpy(d_result, &h_result, sizeof(int), cudaMemcpyHostToDevice);
 
-    printf("answer = %d\n", h);  // 42 기대
+    // 커널 실행: 1개의 블록, 100개의 스레드
+    sumKernel<<<NUM_BLOCKS, NUM_THREADS>>>(d_result);
+
+    // 결과를  CPU로 복사
+    cudaMemcpy(&h_result, d_result, sizeof(int), cudaMemcpyDeviceToHost);
+
+    // GPU 메모리 해제
+    cudaFree(d_result);
+
+    std::cout<<"누적합 결과 = "<<h_result<< std::endl;
     return 0;
 }
